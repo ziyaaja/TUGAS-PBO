501f045c74e5b8569a6a7885b95fc42975e0d9a4
siti fauziyyah <160370622+ziyaaja@users.noreply.github.com>
2025-05-31 17:04:21 +0700

    Create Exception4.java

---
+++ Exception4.java
@@ -0,0 +1,17 @@
+public class Exception4 {
+    public static void main(String[] args) {
+        int bil = 10;
+        String b[] = {"a", "b", "c"};
+
+        try {
+            System.out.println(bil / 0);
+            System.out.println(b[3]);
+        } catch (ArithmeticException e) {
+            System.out.println("Terjadi Aritmatika error");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Melebihi jumlah array");
+        } catch (Exception e) {
+            System.out.println("Ini menghandle error yang terjadi");
+        }
+    }
+}
