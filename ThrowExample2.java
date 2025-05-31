17753bbdb6166a261ddcb37e738bfce151d97d94
siti fauziyyah <160370622+ziyaaja@users.noreply.github.com>
2025-05-31 17:10:57 +0700

    Create ThrowExample2.java

---
+++ ThrowExample2.java
@@ -0,0 +1,13 @@
+public class ThrowExample2 {
+    public static void main(String[] args) {
+        try {
+            throw new Exception("Here's my Exception");
+        } catch (Exception e) {
+            System.out.println("Caught Exception");
+            System.out.println("e.getMessage(): " + e.getMessage());
+            System.out.println("e.toString(): " + e.toString());
+            System.out.println("e.printStackTrace():");
+            e.printStackTrace();
+        }
+    }
+}
